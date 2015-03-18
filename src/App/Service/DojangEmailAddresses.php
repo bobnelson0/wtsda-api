3f2771953bb4f31a5163a5787b2d24fd1f12f8e1
Robert Nelson <bob.nelson@gmail.com>
2015-03-18 11:35:05 -0700

    Behind on committing. Catching up

---
+++ src/App/Service/DojangEmailAddresses.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * User: Robert S. Nelson <bob.nelson@gmail.com>
+ * Date: 2015-02-20
+ * Time: 1:53 PM
+ */
+namespace App\Service;
+
+use App\Service;
+use App\Util\Request;
+
+/**
+ * Class DojangEmailAddresses
+ * @package App\Service
+ */
+class DojangEmailAddresses extends Service
+{
+    /**
+     * Entity path
+     *
+     * $var string
+     */
+    protected $entityPath = 'App\Entity\DojangEmailAddress';
+
+    /**
+     * Entity alias
+     *
+     * $var string
+     */
+    protected $entityAlias = 'doj_em';
+
+    /**
+     * Default filters to apply when searching on this entity
+     *
+     * @var array
+     */
+    protected $defaultFilters = array();
+
+    /**
+     * Default sorts to apply when retrieving this entity
+     *
+     * @var array
+     */
+    protected $defaultSorts = array(array('sort' => 'id', 'dir' => 'asc'));
+
+    /**
+     * @var array
+     */
+    protected static $defaultEntitiesIncluded = array();
+
+    /**
+     * @var array
+     */
+    protected static $defaultEntitiesExcluded = array();
+
+    /**
+     * @param \App\Entity\DojangEmailAddress $data
+     * @param $getRelations
+     * @return array
+     */
+    public static function formatData($data, $getRelations = true) {
+        $formatted = array(
+            'id' => $data->getId(),
+            'type' => $data->getType(),
+            'email' => $data->getEmail(),
+            'created' => $data->getCreated(),
+            'updated' => $data->getUpdated(),
+            'links' => self::formatLink($data, 'dojangAddresses', 'self')
+        );
+
+        return $formatted;
+    }
+}
---
+++ src/App/Service/DojangPhoneNumbers.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * User: Robert S. Nelson <bob.nelson@gmail.com>
+ * Date: 2015-02-20
+ * Time: 1:56 PM
+ */
+
+namespace App\Service;
+
+use App\Service;
+use App\Util\Request;
+
+/**
+ * Class DojangEmailAddresses
+ * @package App\Service
+ */
+class DojangPhoneNumbers extends Service
+{
+    /**
+     * Entity path
+     *
+     * $var string
+     */
+    protected $entityPath = 'App\Entity\DojangPhoneNumber';
+
+    /**
+     * Entity alias
+     *
+     * $var string
+     */
+    protected $entityAlias = 'doj_ph';
+
+    /**
+     * Default filters to apply when searching on this entity
+     *
+     * @var array
+     */
+    protected $defaultFilters = array();
+
+    /**
+     * Default sorts to apply when retrieving this entity
+     *
+     * @var array
+     */
+    protected $defaultSorts = array(array('sort' => 'id', 'dir' => 'asc'));
+
+    /**
+     * @var array
+     */
+    protected static $defaultEntitiesIncluded = array();
+
+    /**
+     * @var array
+     */
+    protected static $defaultEntitiesExcluded = array();
+
+    /**
+     * @param \App\Entity\DojangPhoneNumber $data
+     * @param $getRelations
+     * @return array
+     */
+    public static function formatData($data, $getRelations = true) {
+        $formatted = array(
+            'id' => $data->getId(),
+            'type' => $data->getType(),
+            'phoneNumber' => $data->getNumber(),
+            'created' => $data->getCreated(),
+            'updated' => $data->getUpdated(),
+            'links' => self::formatLink($data, 'dojangPhoneNumbers', 'self')
+        );
+
+        return $formatted;
+    }
+}
